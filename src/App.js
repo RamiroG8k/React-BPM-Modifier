6cf8a805029ce655dc7dfd5191f157d22804e58c
Ramiro Méndez López <=>
2021-07-25 01:24:43 -0500

    init view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+
 const App = () => {
+    const { search } = useLocation();
+    const { token, id } = queryString.parse(search);
+
+    console.log(token, id);
+
     return (
-        <section className="flex h-screen w-screen bg-gray-700 justify-center items-center">
-            <div className="text-center">
-                <h1 className="text-5xl font-semibold text-gray-400">Tailwind x React</h1>
-                <h2><a className="text-gray-500" 
-                href="https://github.com/RamiroG8k" target="_blank" rel="noreferrer">RamiroG8k</a> template</h2>
-            </div>
-        </section>
+        <main className="flex h-screen w-screen bg-gray-200">
+            <section className="flex flex-col w-full sm:w-1/4 h-1/4 m-5 sm:mx-auto bg-white rounded-4xl">
+                <div className="text-center leading-5 mt-4 text-xl">
+                    <h2 className="font-medium">Ahora reproduciendo</h2>
+                    <h3 className="font-bold">Titulo - Artista</h3>
+                </div>
+                <div className="flex h-full justify-center items-center text-center">
+                    <div className="w-1/2">
+                        <p className="font-bold text-8xl text-red-400">169</p>
+                    </div>
+                    <div className="text-left">
+                        <p className="text-5xl font-bold text-gray-700">BPM</p>
+                        <p className="text-3xl font-semibold text-gray-400">90.7%</p>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            screens: {
+                'xs': '320px',
+            },
+            borderRadius: {
+                '4xl': '2.5rem;',
+                '5xl': '3.5rem;'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
