2f0dde2ca13f70cfa7cc43d5fa90c519021bbc35
Ramiro Mendez <ramiromendez@XIFRA-SIS006s-MacBook-Pro.local>
2021-09-30 12:48:26 -0500

    step handler

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,23 +1,23 @@
 import Slider, { SliderTooltip } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
-const ReactRange = ({ onChange, disabled }) => {
+const ReactRange = ({ onChange, disabled, steps }) => {
     const { Handle } = Slider;
 
     const handle = props => {
         const { value, dragging, index, ...restProps } = props;
         return (
-            <SliderTooltip key={index} prefixCls="rc-slider-tooltip" 
-                overlay={`${value} %`} visible={dragging} placement="top">
+            <SliderTooltip key={index} prefixCls="rc-slider-tooltip"
+                overlay={`${value - 100} %`} visible={dragging} placement="top">
                 <Handle value={value} {...restProps} />
             </SliderTooltip>
         );
     };
 
     return (
-        <Slider disabled={disabled} min={50} max={150} defaultValue={100} handle={handle} onChange={onChange}
-        handleStyle={{ height: 20, width: 20, marginTop: -8, backgroundColor: '#F87171', border: 0 }}
-        railStyle={{ height: 4, borderRadius: 50 }} trackStyle={{ background: 'none' }} />
+        <Slider disabled={disabled} min={100 - steps} max={100 + steps} defaultValue={100} handle={handle} onChange={onChange}
+            handleStyle={{ height: 20, width: 20, marginTop: -8, backgroundColor: '#F87171', border: 0 }}
+            railStyle={{ height: 4, borderRadius: 50 }} trackStyle={{ background: 'none' }} />
     );
 
 };
